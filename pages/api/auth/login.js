9758e495dd454c86daead49922478bc0adde02c3
Cesar Tobon <cesar.tobon@finoservices.com>
2023-07-06 12:39:05 -0500

    FiveCommit-ChangesLibraryApp

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,15 +1,19 @@
+import jwt from 'jsonwebtoken'
+
 export default function loginHandler(req, res){
     const {email, password} = req.body
 
     //Validamos que el usuario y la contraseña sean validos
+    if(email === 'admin@library.com' && password === 'adminLibrary123'){
+      const token = jwt.sign({
+        exp: Math.floor(Date.now() /1000) + 60 * 60 * 24 * 30,
+        email: 'admin@library.com', 
+        username: 'library'
+      }, 'secret')
+    }
 
-    
 
-    //Validar si el correo existe 
-
-
-    //Validar si la contraseña si es correcta. 
-
+    res.setHeader('Set-Cookie', '')
     return res.json('login route')
 }
 
